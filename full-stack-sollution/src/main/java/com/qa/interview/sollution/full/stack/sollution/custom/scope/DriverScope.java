363c7e1d7ecabce96fc334d46de709501d0f92cf
SeleniumTestAB <karas.tsukasa@gmail.com>
2022-06-16 19:27:11 +0200

    some scope experiment

--- full-stack-sollution/src/main/java/com/qa/interview/sollution/full/stack/sollution/custom/scope/DriverScope.java
+++ full-stack-sollution/src/main/java/com/qa/interview/sollution/full/stack/sollution/custom/scope/DriverScope.java
@@ -4,12 +4,14 @@ import com.codeborne.selenide.SelenideDriver;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.context.support.SimpleThreadScope;
 
+import java.util.Objects;
+
 public class DriverScope extends SimpleThreadScope {
 
     @Override
     public Object get(String name, ObjectFactory<?> objectFactory) {
         Object o = super.get(name, objectFactory);
-        if(!((SelenideDriver)o).hasWebDriverStarted()){
+        if(Objects.isNull(((SelenideDriver) o).driver().getSessionId()) || !((SelenideDriver)o).hasWebDriverStarted()){
             super.remove(name);
             o = super.get(name, objectFactory);
         }
